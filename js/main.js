4864b349a06763b22bf7cc0140ac547cb4666719
azamatovbobur <azamatovbobur591@gmail.com>
2022-04-10 14:36:12 +0500

    complited

---
+++ js/main.js
@@ -0,0 +1,60 @@
+var GUESS_NUMBER_MAX = 100;
+var GUESS_NUMBER_MIN = 1; 
+var RANDOM_NUMBER = Math.floor(Math.random() * (GUESS_NUMBER_MAX - GUESS_NUMBER_MIN + 1) + GUESS_NUMBER_MIN);
+console.log(RANDOM_NUMBER);
+alert("Salom foydalanuvchi siz hozir random dasturi orqali tanlangan raqamni 5 urinishda topishga harakat qilib ko'rasiz. Xoxlasangiz `OK` tugmasini bosing")
+var number = prompt('Raqam tanlang');
+console.log(number);
+if(number > RANDOM_NUMBER) {
+    alert("Siz tanlagan raqam katta, yana urinib ko'ring");
+}
+else if(number < RANDOM_NUMBER) {
+    alert("Siz tanlagan raqam kichik, yana urinib ko'ring");
+}
+else if(number = RANDOM_NUMBER) {
+    alert("Tabriklaymiz siz Dahosiz");
+}
+var number = prompt('Raqam tanlang');
+console.log(number);
+if(number > RANDOM_NUMBER) {
+    alert("Siz tanlagan raqam katta, yana urinib ko'ring");
+}
+else if(number < RANDOM_NUMBER) {
+    alert("Siz tanlagan raqam kichik, yana urinib ko'ring");
+}
+else if(number = RANDOM_NUMBER) {
+    alert("Tabriklaymiz siz Dahosiz");
+}
+var number = prompt('Raqam tanlang');
+console.log(number);
+if(number > RANDOM_NUMBER) {
+    alert("Siz tanlagan raqam katta, yana urinib ko'ring");
+}
+else if(number < RANDOM_NUMBER) {
+    alert("Siz tanlagan raqam kichik, yana urinib ko'ring");
+}
+else if(number = RANDOM_NUMBER) {
+    alert("Tabriklaymiz siz Dahosiz");
+}
+var number = prompt('Raqam tanlang');
+console.log(number);
+if(number > RANDOM_NUMBER) {
+    alert("Siz tanlagan raqam katta, yana urinib ko'ring");
+}
+else if(number < RANDOM_NUMBER) {
+    alert("Siz tanlagan raqam kichik, yana urinib ko'ring");
+}
+else if(number = RANDOM_NUMBER) {
+    alert("Tabriklaymiz siz Dahosiz");
+}
+var number = prompt('Raqam tanlang');
+console.log(number);
+if(number > RANDOM_NUMBER) {
+    alert("Siz tanlagan raqam katta, Uzr sizda urinishlar qomadi, Ko'proq kitob o'qing!");
+}
+else if(number < RANDOM_NUMBER) {
+    alert("Siz tanlagan raqam kichik, Uzr sizda urinishlar qomadi, Ko'proq kitob o'qing!");
+}
+else if(number = RANDOM_NUMBER) {
+    alert("Tabriklaymiz siz Dahosiz");
+}
